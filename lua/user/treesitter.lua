db40203407937881a4a9188cec5e310fcbb90612
Dustin Heeg <dustinheeg@gmail.com>
2022-10-10 20:04:58 -0500

    Fix a few things for nvim 0.8

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,7 +1,7 @@
 local configs = require('nvim-treesitter.configs')
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
+	ensure_installed = { "sql", "python", "markdown", "markdown_inline", "lua", "html", "json", -- one of "all" or a list of languages
 	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
