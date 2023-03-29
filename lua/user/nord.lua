efcfb1cc9a878fa27b41c8700358110c9bd20384
Dustin Heeg <dustinheeg@gmail.com>
2023-03-29 15:59:44 -0500

    Set the highlight for treesitter context

--- lua/user/nord.lua
+++ lua/user/nord.lua
@@ -1,5 +1,10 @@
-vim.cmd[[colorscheme nord]]
-
 require("nord").setup({
   errors = { mode = "fg" }
 })
+
+vim.cmd[[colorscheme nord]]
+
+-- Override the colors for TreesitterContext.
+local nord_colors = require('nord.colors')
+local cmdstr = "highlight TreesitterContext guibg=" .. nord_colors.palette.polar_night.brightest
+vim.cmd(cmdstr)
