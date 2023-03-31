7786ab7d368e88d921ce506a0a2ef8e4acc83025
Dustin <dustinheeg@gmail.com>
2023-03-30 23:34:01 -0500

    ToggleTerm config change

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -1,7 +1,6 @@
 local toggleterm = require('toggleterm')
 
 toggleterm.setup({
-	size = 20,
 	open_mapping = [[<C-\>]],
 	hide_numbers = true,
 	shade_terminals = true,
@@ -9,20 +8,7 @@ toggleterm.setup({
 	start_in_insert = true,
 	insert_mappings = true,
 	persist_size = true,
-	direction = "float",
+	direction = "horizontal",
 	close_on_exit = true,
 	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
-	},
 })
-
-function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
-end
-
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
