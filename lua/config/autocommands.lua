80e0dceed4fb8c471d2a95e2783e5dd2a99545b3
Dustin Heeg <dustinheeg@gmail.com>
2023-09-06 20:56:11 -0500

    Minor config change

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -20,6 +20,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 -- Highlight Yanked Text
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+    vim.highlight.on_yank { higroup = "Visual", timeout = 300 }
   end,
 })
